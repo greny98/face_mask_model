f5670f4965dba47a584ad6d2b15ec8471e164548
greny98 <20164367@student.hust.edu.vn>
2022-01-12 21:32:51 +0700

    change anchor box large

--- convert_savedmodel.py
+++ convert_savedmodel.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 from model.inference import PredictModel
 
-model = PredictModel(2, large=True, weights="ckpt/fire/checkpoint")
+model = PredictModel(1, large=True, weights="ckpt/fire/checkpoint")
 tf.saved_model.save(model, "saved_model")
